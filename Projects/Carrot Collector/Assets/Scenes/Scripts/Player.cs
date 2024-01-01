43a73cac74aab3809133a243e8d2117ff5649ca4
Gurram Sai Harish <110049566+SaiHarish820@users.noreply.github.com>
2024-01-01 22:48:30 +0530

    Carrot Collector V1 Added

---
+++ Projects/Carrot Collector/Assets/Scenes/Scripts/Player.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public FixedJoystick joystick;
+    public float moveSpeed;
+
+
+    float hInput, vInput;
+
+    int score = 0;
+    public GameObject winText;
+
+    public int winScore;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        hInput = joystick.Horizontal * moveSpeed;
+        vInput = joystick.Vertical * moveSpeed;
+        transform.Translate(hInput, vInput, 0);
+
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+
+        if (other.gameObject.tag == "Carrot")
+        {
+            score++;
+            Destroy(other.gameObject);
+            if(score >= winScore)
+            {
+            winText.SetActive(true);
+            }
+        }
+}
+}
